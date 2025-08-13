697092d024eb101f88d6ee3e341f369d46e179a0
josimuddin198 <josimweb0504@gmail.com>
2025-08-13 18:13:13 +0600

    fix: responsive issue

--- src/components/shared/logo/Logo.jsx
+++ src/components/shared/logo/Logo.jsx
@@ -4,7 +4,10 @@ import { Link } from "react-router";
 
 const Logo = () => {
   return (
-    <Link to="/" className="flex items-center gap-2 cursor-pointer z-50">
+    <Link
+      to="/"
+      className="hidden md:flex items-center gap-2 cursor-pointer z-50"
+    >
       <AiOutlineRead className="md:text-2xl text-sm text-[#00ACC1]" />
       <span className="md:text-2xl text-sm font-bold text-[#00ACC1]">
         StudySphere
